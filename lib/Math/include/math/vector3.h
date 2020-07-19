c6f40c909111058505d4a250473bb526bfbcd82b
Kévin Alexandre Boissonneault <kaboissonneault@gmail.com>
2020-07-18 21:16:50 -0400

    Added vector operations

--- lib/Math/include/math/vector3.h
+++ lib/Math/include/math/vector3.h
@@ -20,6 +20,11 @@ namespace ot::math
 	struct vector3
 	{
 		T x, y, z;
+
+		T norm() const
+		{
+			return static_cast<T>(std::sqrt(x * x + y * y + z * z));
+		}
 	};
 
 	template<typename T>
@@ -35,11 +40,56 @@ namespace ot::math
 	}
 
 	template<typename T>
+	[[nodiscard]] constexpr inline vector3<T> operator+(vector3<T> lhs, vector3<T> rhs) noexcept
+	{
+		return { lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z };
+	}
+
+	template<typename T>
 	constexpr inline point3<T>& operator+=(point3<T>& lhs, vector3<T> rhs) noexcept
 	{
 		lhs.x += rhs.x;
 		lhs.y += rhs.y;
 		lhs.z += rhs.z;
+		return lhs;
+	}
+
+	template<typename T>
+	constexpr inline point3<T>& operator+=(vector3<T>& lhs, vector3<T> rhs) noexcept
+	{
+		lhs.x += rhs.x;
+		lhs.y += rhs.y;
+		lhs.z += rhs.z;
+		return lhs;
+	}
+
+	template<typename T>
+	[[nodiscard]] constexpr inline vector3<T> operator*(vector3<T> lhs, T rhs) noexcept
+	{
+		return { lhs.x * rhs, lhs.y * rhs, lhs.z * rhs };
+	}
+
+	template<typename T>
+	constexpr inline vector3<T>& operator*=(vector3<T>& lhs, T rhs) noexcept
+	{
+		lhs.x *= rhs;
+		lhs.y *= rhs;
+		lhs.z *= rhs;
+		return lhs;
+	}
+
+	template<typename T>
+	[[nodiscard]] constexpr inline vector3<T> operator/(vector3<T> lhs, T rhs) noexcept
+	{
+		return { lhs.x / rhs, lhs.y / rhs, lhs.z / rhs };
+	}
+
+	template<typename T>
+	constexpr inline vector3<T>& operator/=(vector3<T>& lhs, T rhs) noexcept
+	{
+		lhs.x /= rhs;
+		lhs.y /= rhs;
+		lhs.z /= rhs;
 		return lhs;
 	}
 
@@ -67,6 +117,26 @@ namespace ot::math
 	using vector3f = vector3<float>;
 	using vector3d = vector3<double>;
 
+	inline vector3f normalized(vector3f v)
+	{
+		return v / v.norm();
+	}
+
+	inline vector3d normalized(vector3d v)
+	{
+		return v / v.norm();
+	}
+
+	inline bool is_normalized(vector3f v)
+	{
+		return ot::float_eq(v.norm(), 1.0f);
+	}
+
+	inline bool is_normalized(vector3d v)
+	{
+		return ot::float_eq(v.norm(), 1.0);
+	}
+
 	[[nodiscard]] inline bool float_eq(point3f lhs, point3f rhs) noexcept
 	{
 		return ot::float_eq(lhs.x, rhs.x) && ot::float_eq(lhs.y, rhs.y) && ot::float_eq(lhs.z, rhs.z);
@@ -86,4 +156,6 @@ namespace ot::math
 	{
 		return ot::float_eq(lhs.x, rhs.x) && ot::float_eq(lhs.y, rhs.y) && ot::float_eq(lhs.z, rhs.z);
 	}
+
+
 }
