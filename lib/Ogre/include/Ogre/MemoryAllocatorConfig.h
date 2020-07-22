44963b67537bde587df42b29e072a57be4a5671e
Kévin Alexandre Boissonneault <kaboissonneault@gmail.com>
2020-07-21 22:25:13 -0400

    Added allocation functions to Ogre MemoryAllocatorConfig.h

--- lib/Ogre/include/Ogre/MemoryAllocatorConfig.h
+++ lib/Ogre/include/Ogre/MemoryAllocatorConfig.h
@@ -5,3 +5,26 @@
 OT_DETAIL_OGRE_EXTERNAL_INCLUDE_BEGIN
 #include <OgreMemoryAllocatorConfig.h>
 OT_DETAIL_OGRE_EXTERNAL_INCLUDE_END
+
+#include <cassert>
+
+namespace ot::ogre
+{
+	template<Ogre::MemoryCategory Category>
+	struct simd_deleter
+	{
+		void operator()(void* p) const noexcept
+		{
+			OGRE_FREE_SIMD(p, Category);
+		}
+	};
+
+	using unique_geometry_mem = std::unique_ptr<void, simd_deleter<Ogre::MEMCATEGORY_GEOMETRY>>;
+
+	inline [[nodiscard]] unique_geometry_mem allocate_geometry(size_t bytes_n)
+	{
+		auto const ptr = OGRE_MALLOC_SIMD(bytes_n, Ogre::MEMCATEGORY_GEOMETRY);
+		assert(ptr != nullptr);
+		return unique_geometry_mem(ptr);
+	}
+}
