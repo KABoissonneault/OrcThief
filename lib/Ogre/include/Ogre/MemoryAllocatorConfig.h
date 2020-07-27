1dd47c174afa517b0f385c4167d833b19167c791
Kévin Alexandre Boissonneault <kaboissonneault@gmail.com>
2020-07-26 21:55:50 -0400

    Fixed Ogre/MemoryAllocatorConfig.h to add missing dependency. Added Ogre make_unique, with OGRE_NEW / OGRE_DELETE

--- lib/Ogre/include/Ogre/MemoryAllocatorConfig.h
+++ lib/Ogre/include/Ogre/MemoryAllocatorConfig.h
@@ -2,14 +2,35 @@
 
 #include "Ogre/detail/common.h"
 
+#include <algorithm>
 OT_DETAIL_OGRE_EXTERNAL_INCLUDE_BEGIN
 #include <OgreMemoryAllocatorConfig.h>
 OT_DETAIL_OGRE_EXTERNAL_INCLUDE_END
 
 #include <cassert>
+#include <type_traits>
 
 namespace ot::ogre
 {
+	template<typename T>
+	struct deleter
+	{
+		void operator()(T* t) const noexcept 
+		{
+			OGRE_DELETE t;
+		}
+	};
+
+	template<typename T>
+	using unique_ptr = std::unique_ptr<T, deleter<T>>;
+
+	template<typename T, typename... Args>
+	unique_ptr<T> make_unique(Args&&... args)
+	{
+		static_assert(std::is_base_of_v<Ogre::AllocatedObject, T>, "This function is for Ogre types");
+		return unique_ptr<T>{OGRE_NEW T(std::forward<Args>(args)...) };
+	}
+
 	template<Ogre::MemoryCategory Category>
 	struct simd_deleter
 	{
@@ -21,7 +42,7 @@ namespace ot::ogre
 
 	using unique_geometry_mem = std::unique_ptr<void, simd_deleter<Ogre::MEMCATEGORY_GEOMETRY>>;
 
-	inline [[nodiscard]] unique_geometry_mem allocate_geometry(size_t bytes_n)
+	[[nodiscard]] inline unique_geometry_mem allocate_geometry(size_t bytes_n)
 	{
 		auto const ptr = OGRE_MALLOC_SIMD(bytes_n, Ogre::MEMCATEGORY_GEOMETRY);
 		assert(ptr != nullptr);
