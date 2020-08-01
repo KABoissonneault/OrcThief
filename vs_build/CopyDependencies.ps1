ea3a9ad6bc87fa379ec8879c771b735500521e7d
Kévin Alexandre Boissonneault <kaboissonneault@gmail.com>
2020-08-01 12:31:28 -0400

    The OrcThief executable now copies external DLLs (ie: Ogre and SDL2) into the OutputPath

---
+++ vs_build/CopyDependencies.ps1
@@ -0,0 +1,19 @@
+param (
+	[Parameter(Mandatory)]
+	[string]$output_path,
+	[Parameter(Mandatory)]
+	[string]$dependencies_path,
+	[Parameter(ValueFromRemainingArguments)]
+	[string[]]$dependencies
+)
+
+If(!(Test-Path $output_path))
+{
+	New-Item -ItemType Directory -Force -Path $output_path
+}
+
+for ( $i = 0; $i -lt $dependencies.count; $i++ ) {
+	$dependency = $dependencies[$i] 
+	$dependency_path = Join-Path -Path $dependencies_path -ChildPath $dependency 
+	Copy-Item -Path $dependency_path $output_path
+}
