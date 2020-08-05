37992e5b9e3c1028d4f092251b3662ba7df8d981
Kévin Alexandre Boissonneault <kaboissonneault@gmail.com>
2020-08-04 21:21:26 -0400

    More Ogre wrapper headers

--- lib/Ogre/include/Ogre/Components/Overlay/Element.h
+++ lib/Ogre/include/Ogre/Components/Overlay/Element.h
@@ -2,6 +2,8 @@
 
 #include "Ogre/detail/common.h"
 
+#include <algorithm>
+
 OT_DETAIL_OGRE_EXTERNAL_INCLUDE_BEGIN
-#include <OgreOverlayContainer.h>
+#include <OgreOverlayElement.h>
 OT_DETAIL_OGRE_EXTERNAL_INCLUDE_END
