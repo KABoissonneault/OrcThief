100f89c35bb35fe5e545cc9aaa0ac006aa712090
Kévin Alexandre Boissonneault <kaboissonneault@gmail.com>
2020-08-09 21:05:42 -0400

    Added Ogre wrapper headers

---
+++ lib/Ogre/include/Ogre/Components/Hlms/Unlit/Datablock.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "Ogre/detail/common.h"
+
+OT_DETAIL_OGRE_EXTERNAL_INCLUDE_BEGIN
+#include <OgreHlmsUnlitDatablock.h>
+OT_DETAIL_OGRE_EXTERNAL_INCLUDE_END
---
+++ lib/Ogre/include/Ogre/ManualObject2.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "Ogre/detail/common.h"
+
+OT_DETAIL_OGRE_EXTERNAL_INCLUDE_BEGIN
+#include <OgreManualObject2.h>
+OT_DETAIL_OGRE_EXTERNAL_INCLUDE_END
