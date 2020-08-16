9fccd534edf9064f72f51409e92f8e88dc3b59b7
Kévin Alexandre Boissonneault <kaboissonneault@gmail.com>
2020-08-16 19:24:57 -0400

    Added more functions to math::line, lerp, inv_lerp, clamp, project, and clamped_project

--- lib/Math/include/math/line.h
+++ lib/Math/include/math/line.h
@@ -14,5 +14,15 @@ namespace ot::math
 	// Returns only a positive distance
 	double distance(line const& l, point3d p);
 
+	point3d lerp(line const& l, double f);
+	double inv_lerp(line const& l, point3d p);
+	point3d clamp(line const& l, point3d p);
+
+	// Returns a point on the line
+	point3d project(line const& l, point3d p);
+
+	// Returns a point on the line between the two points
+	point3d clamped_project(line const& l, point3d p);
+
 	line transform(line const& l, transformation const& t);
 }
--- lib/Math/src/line.cpp
+++ lib/Math/src/line.cpp
@@ -19,4 +19,38 @@ namespace ot::math
 			transform(l.b, t)
 		};
 	}
+
+	point3d lerp(line const& l, double f)
+	{
+		return l.a + (l.b - l.a) * f;
+	}
+
+	double inv_lerp(line const& l, point3d p)
+	{
+		math::vector3d const v1 = l.b - l.a;
+		math::vector3d const v2 = p - l.a;
+
+		return dot_product(v2, v1) / v1.norm_squared();
+	}
+
+	point3d clamp(line const& l, point3d p)
+	{
+		double const f = inv_lerp(l, p);
+		double const df = std::clamp(f, 0.0, 1.0);
+		return lerp(l, df);
+	}
+
+	point3d project(line const& l, point3d p)
+	{
+		math::vector3d const v1 = l.b - l.a;
+
+		return l.a + v1 * inv_lerp(l, p);
+	}
+
+	point3d clamped_project(line const& l, point3d p)
+	{
+		math::vector3d const v1 = l.b - l.a;
+		double const f = std::clamp(inv_lerp(l, p), 0.0, 1.0);
+		return l.a + v1 * f;
+	}
 }
