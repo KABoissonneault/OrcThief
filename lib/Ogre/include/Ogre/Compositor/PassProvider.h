7bef723a5e59da2d1a0e3d8a5e5e5244154c5a14
Kévin Alexandre Boissonneault <kaboissonneault@gmail.com>
2020-09-06 21:35:35 -0400

    Removed warnings in Ogre Compositor headers

--- lib/Ogre/include/Ogre/Compositor/PassProvider.h
+++ lib/Ogre/include/Ogre/Compositor/PassProvider.h
@@ -1,3 +1,7 @@
 #pragma once
 
+#include "Ogre/detail/common.h"
+
+OT_DETAIL_OGRE_EXTERNAL_INCLUDE_BEGIN
 #include <Compositor/Pass/OgreCompositorPassProvider.h>
+OT_DETAIL_OGRE_EXTERNAL_INCLUDE_END
