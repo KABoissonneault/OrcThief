4442bc6aff4b34a94e68cacc32a8261aacb7b7d3
Kévin Alexandre Boissonneault <kaboissonneault@gmail.com>
2020-09-08 20:26:34 -0400

    Changed uptr<...> to fwd_uptr in accumulator.h

--- src/DwarfEditor/action/accumulator.h
+++ src/DwarfEditor/action/accumulator.h
@@ -11,12 +11,12 @@ namespace ot::dedit::action
 	class accumulator
 	{
 	public:
-		virtual void push_brush_action(uptr<brush_base, fwd_delete<brush_base>> p) = 0;
+		virtual void push_brush_action(fwd_uptr<brush_base> p) = 0;
 
 		template<typename Action, typename... Args>
 		void emplace_brush_action(Args&&... args)
 		{
-			push_brush_action(uptr<brush_base, fwd_delete<brush_base>>(new Action(std::forward<Args>(args)...)));
+			push_brush_action(fwd_uptr<brush_base>(new Action(std::forward<Args>(args)...)));
 		}
 	};
 }
