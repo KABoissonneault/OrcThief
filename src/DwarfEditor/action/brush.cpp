08cb1b6d6f804a89f698b50497492cd593badc4d
Kévin Alexandre Boissonneault <kaboissonneault@gmail.com>
2020-09-25 16:18:12 -0400

    Refactored brush actions to reduce "brush searching" duplication. Added "set_position", "set_rotation", and "set_scale" brush actions

--- src/DwarfEditor/action/brush.cpp
+++ src/DwarfEditor/action/brush.cpp
@@ -13,43 +13,113 @@ namespace ot::dedit::action
 
 	single_brush::single_brush(brush const& b)
 		: id(b.get_id())
+	{
+
+	}
+
+	void single_brush::apply(map& current_map)
+	{
+		brush* b = current_map.find_brush(get_id());
+		if (b == nullptr)
+		{
+			std::fprintf(stderr, "error: could not apply action, entity id '%u' not found\n", static_cast<uint32_t>(get_id()));
+			return;
+		}
+
+		do_apply(*b);
+	}
+
+	void single_brush::undo(map& current_map)
+	{
+		brush* b = current_map.find_brush(get_id());
+		if (b == nullptr)
+		{
+			std::fprintf(stderr, "error: could not undo action, entity id '%u' not found\n", static_cast<uint32_t>(get_id()));
+			return;
+		}
+
+		do_undo(*b);
+	}
+
+	brush_definition_base::brush_definition_base(brush const& b)
+		: single_brush(b)
 		, previous_state(b.mesh_def)
 	{
 
 	}
 
-	void single_brush::undo(map& current_map)
+	void brush_definition_base::do_undo(brush& b)
 	{
-		brush* b = current_map.find_brush(id);
-		if (b == nullptr)
-		{
-			std::fprintf(stderr, "error: could not undo single brush action, entity id '%u' not found\n", static_cast<uint32_t>(id));
-			return;
-		}
-
-		b->reload_node(std::move(previous_state));
+		b.reload_node(std::move(previous_state));
 	}
 
 	split_edge::split_edge(brush const& b, egfx::half_edge::id edge, math::point3f point)
-		: single_brush(b)
+		: brush_definition_base(b)
 		, edge(edge)
 		, point(point)
 	{
 
 	}
 
-	void split_edge::apply(map& current_map)
+	void split_edge::do_apply(brush& b)
 	{
-		brush* b = current_map.find_brush(get_id());
-		if (b == nullptr)
-		{
-			std::fprintf(stderr, "error: could not apply split edge action, entity id '%u' not found\n", static_cast<uint32_t>(get_id()));
-			return;
-		}
-
-		auto new_mesh = std::make_shared<egfx::mesh_definition>(*b->mesh_def);
+		auto new_mesh = std::make_shared<egfx::mesh_definition>(*b.mesh_def);
 		new_mesh->get_half_edge(edge).split_at(point);
-		b->reload_node(std::move(new_mesh));
+		b.reload_node(std::move(new_mesh));
+	}
+
+	set_position::set_position(brush const& b, math::point3f point)
+		: single_brush(b)
+		, previous_state(b.node.get_position())
+		, new_pos(point)
+	{
+
+	}
+
+	void set_position::do_apply(brush& b)
+	{
+		b.node.set_position(new_pos);
+	}
+
+	void set_position::do_undo(brush& b)
+	{
+		b.node.set_position(previous_state);
+	}
+
+	set_rotation::set_rotation(brush const& b, math::quaternion rot)
+		: single_brush(b)
+		, previous_state(b.node.get_rotation())
+		, new_rot(rot)
+	{
+
+	}
+
+	void set_rotation::do_apply(brush& b)
+	{
+		b.node.set_rotation(new_rot);
+	}
+
+	void set_rotation::do_undo(brush& b)
+	{
+		b.node.set_rotation(previous_state);
+	}
+
+	set_scale::set_scale(brush const& b, float s)
+		: single_brush(b)
+		, previous_state(b.node.get_scale())
+		, new_s(s)
+	{
+
+	}
+
+	void set_scale::do_apply(brush& b)
+	{
+		b.node.set_scale(new_s);
+	}
+
+	void set_scale::do_undo(brush& b)
+	{
+		b.node.set_scale(previous_state);
 	}
 }
 
--- src/DwarfEditor/action/brush.h
+++ src/DwarfEditor/action/brush.h
@@ -20,24 +20,78 @@ namespace ot::dedit::action
 	class single_brush : public brush_base
 	{
 		entity_id id;
-		std::shared_ptr<egfx::mesh_definition const> previous_state;
+
 	protected:
 		single_brush(brush const& b);
-		
+
 		entity_id get_id() const noexcept { return id; }
 
+		virtual void do_apply(brush& b) = 0;
+		virtual void do_undo(brush& b) = 0;
+
 	public:
-		virtual void undo(map& current_map) override;
+		virtual void apply(map& current_map) override final;
+		virtual void undo(map& current_map) override final;
 	};
 
-	class split_edge : public single_brush
+	// For actions that change the mesh's definition
+	class brush_definition_base : public single_brush
+	{		
+		std::shared_ptr<egfx::mesh_definition const> previous_state;
+	protected:
+		brush_definition_base(brush const& b);
+
+		virtual void do_undo(brush& b) override;
+	};
+
+	class split_edge : public brush_definition_base
 	{
 		egfx::half_edge::id edge;
 		math::point3f point;
 
+	protected:
+		virtual void do_apply(brush& b) override;
+
 	public:
 		split_edge(brush const& b, egfx::half_edge::id edge, math::point3f point);
+	};
 
-		virtual void apply(map& current_map) override;
+	class set_position : public single_brush
+	{
+		math::point3f previous_state;
+		math::point3f new_pos;
+
+	protected:
+		virtual void do_apply(brush& b) override;
+		virtual void do_undo(brush& b) override;
+
+	public:
+		set_position(brush const& b, math::point3f point);
+	};
+
+	class set_rotation : public single_brush
+	{
+		math::quaternion previous_state;
+		math::quaternion new_rot;
+	
+	protected:
+		virtual void do_apply(brush& b) override;
+		virtual void do_undo(brush& b) override;
+
+	public:
+		set_rotation(brush const& b, math::quaternion rot);
+	};
+
+	class set_scale : public single_brush
+	{
+		float previous_state;
+		float new_s;
+
+	protected:
+		virtual void do_apply(brush& b) override;
+		virtual void do_undo(brush& b) override;
+
+	public:
+		set_scale(brush const& b, float s);
 	};
 }
