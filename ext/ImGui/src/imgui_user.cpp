8a890cfaf648710887242e656d89e1c973d163b3
Kévin Alexandre Boissonneault <kaboissonneault@gmail.com>
2020-10-13 07:56:47 -0400

    Change ImGui Push/Pop ItemDisabled to use a single style vars instead of a bunch of colors for transparency

--- ext/ImGui/src/imgui_user.cpp
+++ ext/ImGui/src/imgui_user.cpp
@@ -13,16 +13,12 @@ namespace ImGui
 		ImGuiStyle& style = g.Style;
 
 		PushItemFlag(ImGuiItemFlags_Disabled, true);
-		PushStyleColor(ImGuiCol_Text, style.Colors[ImGuiCol_Text] * ImVec4(1.0f, 1.0f, 1.0f, 0.5f));
-		PushStyleColor(ImGuiCol_FrameBg, style.Colors[ImGuiCol_FrameBg] * ImVec4(1.0f, 1.0f, 1.0f, 0.5f));
-		PushStyleColor(ImGuiCol_CheckMark, style.Colors[ImGuiCol_CheckMark] * ImVec4(1.0f, 1.0f, 1.0f, 0.5f));
-		PushStyleColor(ImGuiCol_Button, style.Colors[ImGuiCol_Button] * ImVec4(1.0f, 1.0f, 1.0f, 0.5f));
-		PushStyleColor(ImGuiCol_SliderGrab, style.Colors[ImGuiCol_SliderGrab] * ImVec4(1.0f, 1.0f, 1.0f, 0.5f));
+		PushStyleVar(ImGuiStyleVar_Alpha, 0.5f);
 	}
 
 	void PopItemDisabled()
 	{
-		PopStyleColor(5);
+		PopStyleVar();
 		PopItemFlag();
 	}
 }
