606565ea4fe1dab1ada8bd97354e4186069dafed
kaboissonneault <kaboissonneault@gmail.com>
2024-01-06 11:12:42 -0500

    Added a core stdint.h header file

---
+++ lib/Core/include/core/stdint.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "core/size_t.h"
+
+namespace ot
+{
+	using int8_t = signed char;
+	using uint8_t = unsigned char;
+	using int16_t = short;
+	using uint16_t = unsigned short;
+	using int32_t = int;
+	using uint32_t = unsigned;
+	using int64_t = long long;
+	using uint64_t = unsigned long long;
+}
